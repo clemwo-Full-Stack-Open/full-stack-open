98b7377a5f21f43984f5a2a7967631e2c6e6b751
Clemens Wodtke <clemwo@mailbox.org>
2022-01-02 00:19:42 +0100

    Finish exercise 3.6

--- part03/phonebook_backend/index.js
+++ part03/phonebook_backend/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
+const {request, response} = require("express");
 const app = express()
 
 app.use(express.json())
 
-const persons = [
+let persons = [
     {
         "id": 1,
         "name": "Arto Hellas",
@@ -26,8 +27,66 @@ const persons = [
     }
 ]
 
+// get all persons
 app.get('/api/persons', (request, response) => {
     response.send(persons)
+})
+
+// get info about phonebook
+app.get('/info', (request, response) => {
+    response.send(`
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date()}</p>
+    `)
+})
+
+// display information for a single entry
+app.get('/api/persons/:id', (request, response) => {
+    // get id from url
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    persons = persons.filter(person => person.id !== id)
+    console.log(persons)
+    if (person) {
+        response.status(204).end()
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.post('/api/persons/', (request, response) => {
+    const body = request.body
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'Name or number missing'
+        })
+    }
+
+    // if name already exists in phonebook return error
+    if (persons.find(person => person.name === body.name)) {
+        return response.status(400).json({
+            error: 'Name already exists in phonebook'
+        })
+    }
+
+    const newPerson = {
+        "name": body.name,
+        "number": body.number,
+        "id": Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)
+    }
+    persons = persons.concat(newPerson)
+    response.json(newPerson)
 })
 
 const PORT = 3001
